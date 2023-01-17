ee3a7e6731753389d5aef82365beced05de92997
Sayak-Sen <110293682+Sayak-Sen@users.noreply.github.com>
2023-01-17 23:41:37 +0530

    Update Employees.sql

--- java/com/greatlearning/ems/Employees.sql
+++ java/com/greatlearning/ems/Employees.sql
@@ -1,8 +1,11 @@
 create database ems;
 use ems;
+create table employees (id int, first_name varchar(100), last_name varchar(100), email varchar(100));
+
 
 Insert into employees (id, first_name, last_name, email) values (1, 
-"Sayak", "Sen", "john.david@gmail.com");
+"Sayak", "Sen", "sayakhtam2@gmail.com");
+
 Insert into employees (id, first_name, last_name, email) values (2, 
 "Torsha", "Sen", "michael.scott@gmail.com");
 Insert into employees (id, first_name, last_name, email) values (3, 
