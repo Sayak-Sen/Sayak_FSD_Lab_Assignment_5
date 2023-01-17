fe2e696703048cfa749432eba2995b725384ec24
Sayak-Sen <110293682+Sayak-Sen@users.noreply.github.com>
2023-01-17 22:36:08 +0530

    Add files via upload

---
+++ java/com/greatlearning/ems/Employees.sql
@@ -0,0 +1,9 @@
+create database ems;
+use ems;
+
+Insert into employees (id, first_name, last_name, email) values (1, 
+"Sayak", "Sen", "john.david@gmail.com");
+Insert into employees (id, first_name, last_name, email) values (2, 
+"Torsha", "Sen", "michael.scott@gmail.com");
+Insert into employees (id, first_name, last_name, email) values (3, 
+"Hrishita", "Sen", "peter.jackson@gmail.com");
